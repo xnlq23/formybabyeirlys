3ea2f5dc68845ce45b59b0a67f135f03cd08d078
xnlq23 <xavierlele23@gmail.com>
2026-02-12 00:48:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Im gonna send you more detailssss YAYYY",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
